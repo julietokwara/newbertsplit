c1d3f1403aa6f9730724bb5dac80ecc6e281ba9e
Sophia Barton <sophiabarton@DN0a22b13a.SUNet>
2019-03-13 17:07:04 -0700

    prints

--- run_squad.py
+++ run_squad.py
@@ -322,8 +322,9 @@ def convert_examples_to_features(examples, tokenizer, max_seq_length,
             #truncate tokens before sep
             if len(tokens) > max_seq_length:
                 tokens = tokens[:max_seq_length]
-                segment_ids = [:max_seq_length]
-                input_mask = input_mask[:max_seq_length]
+                # segment_ids = [:max_seq_length]
+                # input_mask = input_mask[:max_seq_length]
+            print("segment ids is: {}".format(segment_ids))
 
             tokens.append("[SEP]")
             print("the length of tokens is: {}".format(len(tokens)))
